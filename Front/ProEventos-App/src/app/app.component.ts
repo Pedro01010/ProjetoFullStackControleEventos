2a410d624a293c9f6644fcdb1b775472851033d1
Pedro01010 <pedrodbz16@gmail.com>
2023-11-21 01:21:33 -0300

    ComponentesTeste

--- Front/ProEventos-App/src/app/app.component.ts
+++ Front/ProEventos-App/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { EventosComponent } from "./eventos/eventos.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [CommonModule, RouterOutlet, EventosComponent]
 })
 export class AppComponent {
   title = 'ProEventos-App';
