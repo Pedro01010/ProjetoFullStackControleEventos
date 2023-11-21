99cc2a8668c099422697713ad5704005e49399b3
Pedro01010 <pedrodbz16@gmail.com>
2023-11-21 01:14:47 -0300

    Sua mensagem de commit aqu

---
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-eventos',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './eventos.component.html',
+  styleUrl: './eventos.component.scss'
+})
+export class EventosComponent {
+
+}
