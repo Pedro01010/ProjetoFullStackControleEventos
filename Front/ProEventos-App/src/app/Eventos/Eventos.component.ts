6c8a4793b5492afbdd40ab93a2aa94d5fbac6d85
Pedro01010 <pedrodbz16@gmail.com>
2023-12-08 01:02:00 -0300

    Angular binding

--- Front/ProEventos-App/src/app/Eventos/Eventos.component.ts
+++ Front/ProEventos-App/src/app/Eventos/Eventos.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-Eventos',
@@ -7,23 +8,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./Eventos.component.scss']
 })
 export class EventosComponent implements OnInit {
+  isCollapsed = false;
+  public eventos: any = [];
+  public eventosFiltrados :any =[];
+  widhtImage:number = 150;
+  marginImage:number = 2;
+  exibirImagem: boolean =true;
+  exibirBotao: boolean =true;
+  private _filtroLista: string = '';
 
-  public eventos: any;
-numRows: any;
-numCols: any;
-gridCards: any;
+  public get filtroLista(): string{
+    return this._filtroLista;
+  }
 
+  public set filtroLista(value: string){
+   this._filtroLista = value
+   this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
+  }
+
+  filtrarEventos(Filtrarpor: string) : any{
+  Filtrarpor= Filtrarpor.toLocaleLowerCase();
+  return this.eventos.filter(
+    (   evento: { tema: string; }) => evento.tema.toLocaleLowerCase().indexOf(Filtrarpor) !== -1
+  )
+  }
   constructor(private http:HttpClient) { }
 
   ngOnInit():void {
 
     this.getEventos();
   }
+  alterarImagem(){
+    this.exibirImagem=!this.exibirImagem;
+  }
+  alterarBotao(){
+    this.exibirBotao=!this.exibirBotao;
+  }
    public getEventos():void{
     this.http.get('https://localhost:5001/api/eventos').subscribe(
-      response => this.eventos = response,
-      error => console.log(error)
+
+      response => {
+        this.eventos = response;
+        this.eventosFiltrados=this.eventos;
+       },
+      error => console.log(error),
+
     );
+
 
 
    }
--- Front/ProEventos-App/src/app/app.module.ts
+++ Front/ProEventos-App/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http'
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -24,7 +25,8 @@ import { CollapseModule } from 'ngx-bootstrap/collapse';
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    CollapseModule.forRoot()
+    CollapseModule.forRoot(),
+    FormsModule
 
   ],
   providers: [],
